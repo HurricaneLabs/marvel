aa21fc7ff5c6be3ef8623be43e952302bb065515
Ian Gillespie <ian@hurricanelabs.com>
2019-03-08 09:20:12 -0500

    Update marvel_comics.js -- updates.

--- bin/app/marvel_comics.js
+++ bin/app/marvel_comics.js
@@ -3,13 +3,13 @@
     var path = require("path");
     var splunkjs = require("splunk-sdk");
     var Marvel = require('marvel');
-    var MarvelPasswords = require('./MarvelPasswords');
     var ModularInputs = splunkjs.ModularInputs;
     var Logger = ModularInputs.Logger;
     var Event = ModularInputs.Event;
     var Scheme = ModularInputs.Scheme;
     var Argument = ModularInputs.Argument;
     var utils = ModularInputs.utils;
+    var MarvelPasswords = require('./marvel_passwords');
     var getPasswords = MarvelPasswords.GetPasswords;
 
     exports.getScheme = function () {
@@ -30,38 +30,35 @@
             new Argument({
                 name: "result_limit",
                 dataType: Argument.dataTypeNumber,
-                description: "Limit the results returned (Minimum: 1, Maximum: 100)",
+                description: "Limit the result returned (Minimum: 1, Maximum: 100)",
                 requiredOnCreate: true,
                 requiredOnEdit: false
-            })
+            }),
         ];
-
-        Logger.info('Comic Scheme: ', scheme);
-
         return scheme;
     };
 
     exports.validateInput = function (definition, done) {
+
         var comic = definition.parameters.comic;
         var result_limit = definition.parameters.result_limit;
-        var service = new splunkjs.Service({sessionKey: definition.metadata["session_key"]});
+        var service = new splunkjs.Service({ sessionKey : definition.metadata["session_key"] });
 
-        if (result_limit < 1) {
+
+        if(result_limit < 1) {
             done(new Error("The minimum amount of results allowed is 1."));
         }
 
-        if (result_limit > 100) {
+        if(result_limit > 100) {
             done(new Error("The maximum amount of results allowed is 100."));
         }
 
         getPasswords(service).then(function(passwords) {
 
             var marvel = new Marvel({
-                publicKey: passwords["public_key"],
-                privateKey: passwords["private_key"]
+                publicKey: passwords['public_key'],
+                privateKey: passwords['private_key']
             });
-
-            Logger.info("Marvel: ", marvel);
 
             marvel.comics
                 .title(comic)
@@ -69,11 +66,10 @@
                     if (err) {
                         Logger.error(name, "A validation error occurred: " + err.message);
                         callback(err);
-                    }
-                    else {
-                        Logger.info("Response: ", res);
+                    } else {
                         if (res.length === 0) {
-                            done(new Error("No results returned for that comic, please try again."));
+                            done(new Error("No results returned for that comic," +
+                                " please try again."));
                         } else {
                             done();
                         }
@@ -86,100 +82,87 @@
 
     exports.streamEvents = function (name, singleInput, eventWriter, done) {
         var checkpointDir = this._inputDefinition.metadata["checkpoint_dir"];
+        var service = new splunkjs.Service({ sessionKey : this._inputDefinition.metadata["session_key"] });
         var comic = singleInput.comic;
         var result_limit = singleInput.result_limit;
-        var service = new splunkjs.Service({sessionKey: this._inputDefinition.metadata["session_key"]});
 
         getPasswords(service).then(function(passwords) {
 
             var marvel = new Marvel({
-                publicKey: passwords["public_key"],
-                privateKey: passwords["private_key"]
+                publicKey: passwords['public_key'],
+                privateKey: passwords['private_key']
             });
 
             var alreadyIndexed = 0;
             var errorFound = false;
 
-            marvel.comics
-                .title(comic)
-                .limit(result_limit)
-                .get(function (err, res) {
-                    if (err) {
-                        Logger.error(name, "An error occurred: " + err.message);
-                        callback(err);
-                        return;
-                    }
+            marvel.comics.title(comic).limit(result_limit).get(function (err, res) {
+                if (err) {
+                    callback(err);
+                }
 
-                    var checkpointFile = path.join(checkpointDir, comic + ".txt");
-                    var checkpointFileNewContents = "";
-                    var checkpointFileContents = "";
+                var checkpointFilePath = path.join(checkpointDir, comic + ".txt");
+                var checkpointFileNewContents = "";
+                var checkpointFileContents = "";
 
-                    Logger.info(name, "Retrieving Marvel comic information: " + res);
+                try {
+                    checkpointFileContents = utils.readFile("", checkpointFilePath);
+                } catch (e) {
+                    fs.appendFileSync(checkpointFilePath, "");
+                }
 
-                    try {
-                        checkpointFileContents = utils.readFile("", checkpointFile);
-                    }
-                    catch (e) {
-                        // If there's an exception, assume the file doesn't exist
-                        // Create the checkpoint file with an empty string
-                        fs.appendFileSync(checkpointFile, "");
-                    }
+                for (var i = 0; i < res.length && !errorFound; i++) {
+                    var json = {
+                        id: res[i].id,
+                        title: res[i].title,
+                        issueNumber: res[i].issueNumber,
+                        description: res[i].description,
+                        modified: res[i].modified,
+                        thumbnail_path: res[i].thumbnail.path,
+                        thumbnail_extension: res[i].thumbnail.extension
+                    };
 
-                    for (var i = 0; i < res.length && !errorFound; i++) {
+                    if (checkpointFileContents.indexOf(res[i].id += res[i].modified + "\n") < 0) {
+                        try {
+                            var event = new Event({
+                                stanza: comic,
+                                sourcetype: "marvel_comics",
+                                data: json,
+                                time: Date.now()
+                            });
 
-                        var json = {
-                            id: res[i].id,
-                            title: res[i].title,
-                            modified: res[i].modified,
-                            issueNumber: res[i].issueNumber,
-                            description: res[i].description,
-                            thumbnail_path: res[i].thumbnail.path,
-                            thumbnail_extension: res[i].thumbnail.extension
-                        };
+                            eventWriter.writeEvent(event);
 
-                        Logger.info(name, "Data returned for " + comic + ": " + json);
+                            checkpointFileNewContents += res[i].id += res[i].modified + "\n";
 
-                        if (checkpointFileContents.indexOf(res[i].id += res[i].modified + "\n") < 0) {
-                            try {
-                                var event = new Event({
-                                    stanza: comic,
-                                    sourcetype: "marvel_comics",
-                                    data: json, // Have Splunk index our event data as JSON, if data is an object it will be passed through JSON.stringify()
-                                    time: Date.now() // Set the event timestamp to the time of the search
-                                });
+                            Logger.info(name, "Added a new comic: " + res[i].name);
 
-                                eventWriter.writeEvent(event);
-
-                                checkpointFileNewContents += res[i].id += res[i].modified + "\n"; // Append this commit to the string we'll write at the end
-                                Logger.info(name, "Added a new comic: " + res[i].title);
-                            }
-                            catch (e) {
-                                errorFound = true;
-                                Logger.error(name, "An error occured: " + e.message);
-                                done(e);
-
-                                // We had an error, die.
-                                return;
-                            }
-                        } else {
-                            alreadyIndexed++;
+                        } catch (e) {
+                            errorFound = true;
+                            Logger.error(name, "An error occured: " + e.message);
+                            done(e);
                         }
-
-                        fs.appendFileSync(checkpointFile, checkpointFileNewContents); // Write to the checkpoint file
-
-                        if (alreadyIndexed > 0) {
-                            Logger.info(name, "Skipped " + alreadyIndexed.toString() + " already indexed the comic" + comic);
-                        }
-
-                        alreadyIndexed = 0;
-
+                    } else {
+                        alreadyIndexed++;
                     }
-                });
+
+                }
+
+                fs.appendFileSync(checkpointFilePath, checkpointFileNewContents);
+
+                if (alreadyIndexed > 0) {
+                    Logger.info(name, "Skipped " + alreadyIndexed.toString() +
+                        " already indexed the character" + character);
+                }
+
+                alreadyIndexed = 0;
+
+            });
         }).catch(function(err) {
             done(new Error(err));
         });
-
     };
 
     ModularInputs.execute(exports, module);
+
 })();
