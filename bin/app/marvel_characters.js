e48b64c80b0f4d23ef7f29bc8f5c13db04027c54
Ian Gillespie <ian@hurricanelabs.com>
2019-03-08 09:19:30 -0500

    Update marvel_characters.js -- updates.

--- bin/app/marvel_characters.js
+++ bin/app/marvel_characters.js
@@ -3,13 +3,13 @@
     var path = require("path");
     var splunkjs = require("splunk-sdk");
     var Marvel = require('marvel');
-    var MarvelPasswords = require('./MarvelPasswords');
     var ModularInputs = splunkjs.ModularInputs;
     var Logger = ModularInputs.Logger;
     var Event = ModularInputs.Event;
     var Scheme = ModularInputs.Scheme;
     var Argument = ModularInputs.Argument;
     var utils = ModularInputs.utils;
+    var MarvelPasswords = require('./marvel_passwords');
     var getPasswords = MarvelPasswords.GetPasswords;
 
     exports.getScheme = function () {
@@ -26,137 +26,122 @@
                 description: "Comic character such as The Incredible Hulk",
                 requiredOnCreate: true,
                 requiredOnEdit: false
-            })
+            }),
         ];
         return scheme;
     };
 
     exports.validateInput = function (definition, done) {
+
         var character = definition.parameters.character;
-        var service = new splunkjs.Service({sessionKey: definition.metadata["session_key"]});
+        var service = new splunkjs.Service({ sessionKey : definition.metadata["session_key"] });
 
         getPasswords(service).then(function(passwords) {
-
             var marvel = new Marvel({
-                publicKey: passwords["public_key"],
-                privateKey: passwords["private_key"]
+                publicKey: passwords['public_key'],
+                privateKey: passwords['private_key']
             });
 
             marvel.characters
-                .name(character)
-                .get(function (err, res) {
-                        if (err) {
-                            Logger.error(name, "A validation error occurred: " + err.message);
-                        }
-                        else {
-                            Logger.info("Response: ", res);
-                            if (res.length === 0) {
-                                done(new Error("No results returned for that series, please try again."));
-                            } else {
-                                done();
-                            }
-                        }
+            .name(character)
+            .get(function (err, res) {
+                if (err) {
+                    Logger.error(name, "A validation error occurred: " + err.message);
+                    callback(err);
+                } else {
+                    if (res.length === 0) {
+                        done(new Error("No results returned for that series," +
+                            " please try again."));
+                    } else {
+                        done();
                     }
-                );
+                }
+            });
         }).catch(function(err) {
             done(new Error(err));
         });
-
     };
 
     exports.streamEvents = function (name, singleInput, eventWriter, done) {
         var checkpointDir = this._inputDefinition.metadata["checkpoint_dir"];
-        var service = new splunkjs.Service({sessionKey: this._inputDefinition.metadata["session_key"]});
+        var service = new splunkjs.Service({ sessionKey : this._inputDefinition.metadata["session_key"] });
         var character = singleInput.character;
 
         getPasswords(service).then(function(passwords) {
-
             var marvel = new Marvel({
-                publicKey: passwords["public_key"],
-                privateKey: passwords["private_key"]
+                publicKey: passwords['public_key'],
+                privateKey: passwords['private_key']
             });
 
             var alreadyIndexed = 0;
             var errorFound = false;
 
-            marvel.characters
-                .name(character).get(function (err, res) {
+            marvel.characters.name(character).get(function (err, res) {
                 if (err) {
-                    Logger.error(name, "An error occurred: " + err.message);
-                    return;
+                    callback(err);
                 }
 
-                var checkpointFile = path.join(checkpointDir, character + ".txt");
+                var checkpointFilePath = path.join(checkpointDir, character + ".txt");
                 var checkpointFileNewContents = "";
                 var checkpointFileContents = "";
 
-                Logger.info(name, "Retrieving Marvel character information: " + res);
-
                 try {
-                    checkpointFileContents = utils.readFile("", checkpointFile);
-                }
-                catch (e) {
-                    // If there's an exception, assume the file doesn't exist
-                    // Create the checkpoint file with an empty string
-                    fs.appendFileSync(checkpointFile, "");
+                    checkpointFileContents = utils.readFile("", checkpointFilePath);
+                } catch (e) {
+                    fs.appendFileSync(checkpointFilePath, "");
                 }
 
                 for (var i = 0; i < res.length && !errorFound; i++) {
-
                     var json = {
                         id: res[i].id,
                         name: res[i].name,
-                        modified: res[i].modified,
                         description: res[i].description,
+                        modified: res[i].modified,
                         thumbnail_path: res[i].thumbnail.path,
                         thumbnail_extension: res[i].thumbnail.extension
                     };
-
-                    Logger.info(name, "Data returned for " + character + ": " + json);
 
                     if (checkpointFileContents.indexOf(res[i].id += res[i].modified + "\n") < 0) {
                         try {
                             var event = new Event({
                                 stanza: character,
                                 sourcetype: "marvel_characters",
-                                data: json, // Have Splunk index our event data as JSON, if data is an object it will be passed through JSON.stringify()
-                                time: Date.now() // Set the event timestamp to the time of the search
+                                data: json,
+                                time: Date.now()
                             });
 
                             eventWriter.writeEvent(event);
 
-                            checkpointFileNewContents += res[i].id += res[i].modified + "\n"; // Append this commit to the string we'll write at the end
+                            checkpointFileNewContents += res[i].id += res[i].modified + "\n";
+
                             Logger.info(name, "Added a new character: " + res[i].name);
-                        }
-                        catch (e) {
+
+                        } catch (e) {
                             errorFound = true;
                             Logger.error(name, "An error occured: " + e.message);
                             done(e);
-
-                            // We had an error, die.
-                            return;
                         }
                     } else {
                         alreadyIndexed++;
                     }
 
-                    fs.appendFileSync(checkpointFile, checkpointFileNewContents); // Write to the checkpoint file
-
-                    if (alreadyIndexed > 0) {
-                        Logger.info(name, "Skipped " + alreadyIndexed.toString() + " already indexed the character " + character);
-                    }
-
-                    alreadyIndexed = 0;
-
                 }
 
-            });
+                fs.appendFileSync(checkpointFilePath, checkpointFileNewContents);
 
+                if (alreadyIndexed > 0) {
+                    Logger.info(name, "Skipped " + alreadyIndexed.toString() +
+                    " already indexed the character" + character);
+                }
+
+                alreadyIndexed = 0;
+
+            });
         }).catch(function(err) {
             done(new Error(err));
         });
-
     };
 
     ModularInputs.execute(exports, module);
+
 })();
